122012292497587f5b9ac6be73312a7960daf7d3
Xu Chen <466894670@qq.com>
2018-11-14 20:14:20 +0000

    Data gathering

--- Project_Version1.py
+++ Project_Version1.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import numpy as np
+
+from pandas_datareader import data as pdr 
+import fix_yahoo_finance as yf 
+
+yf.pdr_override()
+
+data =  pdr.get_data_yahoo("GOOG", start = "2018-01-01", end = "2018-06-30")
+
+print(data.Open.describe())
