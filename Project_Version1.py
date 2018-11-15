37078734fd934b9fe877c1e0e57a6e362f045e21
MrMandlaNyathi <45071433+MrMandlaNyathi@users.noreply.github.com>
2018-11-15 14:43:38 +0000

    4 Different Ways to load data; Data Descriptive Stats; Time Series Plot; Linear Regression Parameters; ARIMA Model Fit

--- Project_Version1.py
+++ Project_Version1.py
@@ -37,3 +37,167 @@ def describe_stock(y):
 
 close.Close.plot()
 plt.show()
+
+# Importing Packages
+
+import pandas as pd
+import statsmodels.api as sm
+from os.path import exists
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+from pylab import rcParams
+import warnings
+import itertools
+import math
+import datetime as dt
+
+# Plot Styles
+
+plt.style.use('fivethirtyeight')
+
+# Opening Statement 
+
+Today = dt.date.today()
+
+print(f"""\n\nThis program is for a daily series as at {Today}, the price of a stock used is the
+closing price, which is the last transaction price of the stock in a trading day.\n""")
+
+# The Menu for the client
+
+print("=" * 100, "\n")
+
+print("Please advise your preferred form of loading data? :\n")
+print("Option :1 - Loading the data through Copy and Paste. \nOption :2 - Loading the data\
+ through a file. \nOption :3 - Loading the Data from a website. \nOption :4 - To quit).", end='\n')
+DLoad = int(input('\n       :  > .....'))
+
+print("\n", "=" * 100, "\n")
+  
+while DLoad != 4:
+        
+    if DLoad == 1       :
+              
+        print("\nPlease copy the data in your file now & Key 1 to confirm having copied your file", end='\n')
+        DLoad = int(input('\n       :  > .....'))
+                
+        mydata = pd.read_clipboard(header=0, parse_dates=[0], index_col=0, squeeze=True)
+        print("\nThank You, This is the 1st few lines of your data\n")
+        print(mydata.head())
+        DLoad = int(input('\nInput 4 to affirm completion   :  > .....'))
+            
+    elif DLoad == 2     :
+                
+        #FName = ("daily_adjusted_MSFT")
+        FName = input("\nPlease state file name : ... ")                      # TODO Remove when going on production
+                
+        if exists(FName) == False :                                             # To test availability of Data 
+            print(f"\nConfirmed, Your File is Available")
+        else                        :
+            print("\nWe do not have data on this File, Please key in an alternative file", end = "")
+            FName = input("\nPlease state file name : ... ")   
+                
+        mydata = pd.read_csv(FName + ".csv", header=0, parse_dates=['timestamp'], squeeze=True)
+        
+        print("\nThank You, This is the 1st few lines of your data\n")
+        print(mydata.head())
+        DLoad = int(input('\nInput 4 to affirm completion   :  > .....'))
+            
+    elif DLoad == 3     :
+                
+        Dweb = input("\nPlease state file location : ... ")
+        mydata = pd.read_csv(Dweb + ".csv", header=0, parse_dates=[0], index_col=0, squeeze=True)
+        print("\nThank You, This is the 1st few lines of your data\n")
+        print(mydata.head())
+        DLoad = int(input('\nInput 4 to affirm completion   :  > .....'))
+    
+    elif DLoad == 4     : break                                                 # TODO
+    
+print("\n", "=" * 100, "\n")
+
+# Data Types 
+
+print("\n", "=" * 100, "\n")
+
+print("\nThe types of data you have per column is : \n")
+print(mydata.dtypes)                                                           # To check data types
+
+# Index Slicing 
+
+idx = pd.IndexSlice
+#mydat = mydata.loc[idx['2018-01-11':'2018-05-11'], :]
+#mydata["timestamp"] = mydata.index.astype("datetime64[ns]")
+#print(mydat)
+# mydat.loc[idx['2018-01-11']:]                                                 # All Data until 1 Nov 2018 (End Date)
+
+# For capturing the start date and End dates                                    # TODO
+
+#df = 0
+#df = pd.DataFrame({'year': [2015, 2016], 'month': [2, 3],'day': [4, 5]})
+#pd.to_datetime(df[['year', 'month', 'day']])
+#pd.to_datetime(df)
+
+# Descriptive Stats of the Data 
+
+print("\nThe descriptive statistics of your data : \n")
+mydat = mydata.close[0:len(mydata)]
+
+print(mydat.describe(), end = "")	                                             # To get descriptive stats
+print("\n")
+print("\n", "=" * 100, "\n")
+
+## Max and Mix Rows 
+
+print("\nThis is your maximum Price from the data : ", mydata.close.max())
+
+Pmax = mydata.close.max()
+print("\nThe complete details for the highest price point", mydata.iloc[np.where(mydata.close == Pmax)])
+
+print("\nThis is your minimum Price from the data : ", mydata.close.min())
+
+Pmin = mydata.close.min()
+print("The complete details for the lowest price point", mydata.iloc[np.where(mydata.close == Pmin)])
+print("\n", "=" * 100, "\n")
+
+## Plotting the Data                  TODO Review
+
+print('\nThe Graph of Your Data Looks Like\n')
+plot = mydata.close.plot(figsize = (15,6))
+plot.set_xlabel("Days")
+plot.set_ylabel("Closing Share Price")
+plot.set_title("Share Price Reported per Day")
+plot.get_figure().savefig("Closing_Price.pdf")
+plt.show()
+print("\n", "=" * 100, "\n")
+
+#
+#mydata      = mydata.set_index(pd.DatetimeIndex(mydata['timestamp']))
+y           = mydata.close[0:len(mydata)]
+##my_start    = pd.to_datetime('2018-07-01')
+#my_start    = ('2018-07-01')
+#my_start
+#print(type(my_start))
+#
+x           =  mydata["timestamp"].astype("datetime64[ns]")
+
+## Linear Regression                    TODO Review
+
+print('\nA Liear Regression Params of Your Data Looks Like\n')
+y = mydata.close[:]
+x = range(len(mydata))
+x = sm.add_constant(x)
+results = sm.OLS(y,x).fit()
+print('\nThese are the Result Parameters\n')
+print(results.params,"\n")
+print("\n", "=" * 100, "\n")
+
+# Running an ARIMA Model
+
+print("\nRunning an Autoregressive Model - 'ARIMA'")
+model = sm.tsa.statespace.SARIMAX(y, order=(1, 1, 1))    	                # Running an ARIMA (1, 1, 1)
+results = model.fit()                                                           # Fitting 
+print(results.summary().tables[1]) 				                                # To get the table of coefficients
+results.plot_diagnostics(figsize=(15, 12))			                            # Diagnostic plot
+plt.show()
+print("\n", "=" * 100, "\n")
+
